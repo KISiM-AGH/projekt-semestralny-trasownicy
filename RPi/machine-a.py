20b87ef776e310be105a9965fdd59266dfd8548d
mmikrut1 <matti.emem@gmail.com>
2020-11-10 19:20:48 +0100

    added listening, machine simulation etc.

--- RPi/machine-a.py
+++ RPi/machine-a.py
@@ -2,16 +2,18 @@ import paho.mqtt.client as mqtt
 import random, threading, json
 from datetime import datetime
 import sys
-import math
 
 # ====================================================
 # MQTT Settings
 MQTT_Broker = "192.168.23.51"
 MQTT_Port = 1883
 Keep_Alive_Interval = 45
-MQTT_Topic = "cloud2020/trasownicy/sensor_data"
-
+MQTT_Topic_send = sys.argv[1]
+MQTT_Topic_listen = sys.argv[2]
+power = 0
+bottles = 0
 # ====================================================
+
 
 def on_connect(client, userdata, rc):
     if rc != 0:
@@ -19,9 +21,27 @@ def on_connect(client, userdata, rc):
         print("Unable to connect to MQTT Broker...")
     else:
         print("Connected with MQTT Broker: " + str(MQTT_Broker))
+    mqttc.subscribe(MQTT_Topic_listen, 0)
 
 
 def on_publish(client, userdata, mid):
+    pass
+
+
+def on_message(mosq, obj, msg):
+    global power
+
+    print("MQTT Data Received...")
+    print("MQTT Topic: " + msg.topic)
+    print("Data: ", msg.payload)
+
+    msg = int(msg.payload)
+    if msg >= 0 & msg <= 100:
+        power = msg
+
+
+def on_subscribe(mosq, obj, mid, granted_qos):
+    print("subscribed")
     pass
 
 
@@ -31,9 +51,11 @@ def on_disconnect(client, userdata, rc):
 
 
 mqttc = mqtt.Client()
+mqttc.on_message = on_message
 mqttc.on_connect = on_connect
 mqttc.on_disconnect = on_disconnect
 mqttc.on_publish = on_publish
+mqttc.on_subscribe = on_subscribe
 mqttc.connect(MQTT_Broker, int(MQTT_Port), int(Keep_Alive_Interval))
 
 
@@ -48,28 +70,32 @@ def publish_To_Topic(topic, message):
 # Dummy code used as Fake Sensor to publish some random values
 # to MQTT Broker
 
+def simulate_machine_a():
+    global power
+    global bottles
+    if power == 0:
+        bottles = 0
+    else:
+        mu = power*0.1
+        sigma = 0.7
+        bottles = int(round(random.gauss(mu, sigma)))
+        if bottles <= 0:
+            bottles = 0
+
 
 def publish_Fake_Sensor_Values_to_MQTT():
-    loc_radius = 50
-    lat = 50.069408
-    lng = 19.904793
 
-    threading.Timer(3.0, publish_Fake_Sensor_Values_to_MQTT).start()
-    humidity_val = float("{0:.2f}".format(random.uniform(50, 90)))
-    temp_val = float("{0:.2f}".format(random.uniform(20, 60)))
-    pollution_val = float("{0:.2f}".format(random.uniform(15, 65)))
-
-    Weather_Data = {}
-    Weather_Data['Sensor_ID'] = sys.argv[1]
-    Weather_Data['Date'] = (datetime.today()).strftime("%d-%b-%Y %H:%M:%S:%f")
-    Weather_Data['Humidity'] = humidity_val
-    Weather_Data['Temperature'] = temp_val
-    Weather_Data['Pollution'] = pollution_val
-    json_data = json.dumps(Weather_Data)
-
-    publish_To_Topic(MQTT_Topic, json_data)
+    threading.Timer(10.0, publish_Fake_Sensor_Values_to_MQTT).start()
+    simulate_machine_a()
+    bottles_data = {'Machine_ID': sys.argv[3],
+                    'Date': (datetime.today()).strftime("%d-%b-%Y %H:%M:%S:%f"),
+                    'Bottles': bottles}
+    json_data = json.dumps(bottles_data)
+    publish_To_Topic(MQTT_Topic_send, json_data)
 
 
 publish_Fake_Sensor_Values_to_MQTT()
 
-# ====================================================
+# Continue the network loop
+mqttc.loop_forever()
+
