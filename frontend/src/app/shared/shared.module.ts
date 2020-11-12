50341a57bd8d1a8fcded2206f127061ffd9ed533
nastopendo <grzegorzlipski97@gmail.com>
2020-11-12 21:00:24 +0100

    added modules structure2

---
+++ frontend/src/app/shared/shared.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AreaComponent } from './widgets/area/area.component';
+
+
+
+@NgModule({
+  declarations: [AreaComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
