e28db61965c1df820a3bae15bc153047970f9b7d
nastopendo <grzegorzlipski97@gmail.com>
2020-11-16 01:37:23 +0100

    added compare-widget options

--- frontend/src/app/shared/widgets/compare/compare.component.ts
+++ frontend/src/app/shared/widgets/compare/compare.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import * as Highcharts from 'highcharts';
+import HC_exporting from 'highcharts/modules/exporting';
+import more from 'highcharts/highcharts-more';
 
 @Component({
-  selector: 'app-compare',
+  selector: 'app-widget-compare',
   templateUrl: './compare.component.html',
   styleUrls: ['./compare.component.css']
 })
-export class CompareComponent implements OnInit {
+export class CompareComponent implements OnInit, OnChanges {
 
-  constructor() { }
+  chartOptions;
+  // @Input() title = '';
+  // @Input() Ytitle = '';
+  // @Input() FactoryID = '';
+  // @Input() hourLabels = [];
+  // @Input() hourBottles = [];
+  // @Input() hourFaults = [];
+
+  Highcharts = Highcharts;
 
   ngOnInit(): void {
+    HC_exporting(Highcharts);
+
+    setTimeout(() => {
+      window.dispatchEvent(
+        new Event('resize')
+      );
+    }, 300);
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    this.chartOptions = {};
+  }
 }
