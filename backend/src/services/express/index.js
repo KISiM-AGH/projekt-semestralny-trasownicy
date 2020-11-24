2a22f5414b8aa40748aaf19c04473ef46ff0c47c
mmikrut1 <matti.emem@gmail.com>
2020-11-24 12:11:43 +0100

    added helmet to handle http headers

--- backend/src/services/express/index.js
+++ backend/src/services/express/index.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const helmet = require('helmet')
 
 const expressConfig = (apiRoot, routes) => {
   const app = express()
-
   const corsOptions = {
     origin: 'https://koko-kola.xyz/',
     optionsSuccessStatus: 200
   }
+
+  app.use(helmet())
   app.use(cors(corsOptions));
   app.use(bodyParser.json())
   app.use(apiRoot, routes)
